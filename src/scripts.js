d490fd6d8862174f1f4c45301f2eac2f0e75090f
Adair Juneo <adair_juneo@hotmail.com>
2021-11-15 20:52:23 -0300

    Menu mobile e titulos de página prontos.

---
+++ src/scripts.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('#header section')
+const openMenu = document.querySelectorAll('section .open-menu')
+const closeMenu = document.querySelectorAll('section .close-menu')
+
+for(const abrirMenu of openMenu){
+    abrirMenu.addEventListener('click', () =>{
+        menu.classList.toggle('active')
+        document.body.style.overflow = "hidden"
+    })
+}
+
+for(const fecharMenu of closeMenu){
+    fecharMenu.addEventListener('click', () =>{
+        menu.classList.toggle('active')
+        document.body.style.overflow = "visible"
+    })
+}
