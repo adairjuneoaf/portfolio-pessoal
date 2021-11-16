976422800ae28d43457763e86585b6389d030dd8
Adair Juneo <adair_juneo@hotmail.com>
2021-11-16 13:53:17 -0300

    Add scrollReveal na pag INICIO.

--- src/scripts.js
+++ src/scripts.js
@@ -15,3 +15,29 @@ for(const fecharMenu of closeMenu){
         document.body.style.overflow = "visible"
     })
 }
+
+const scrollTop = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 500,
+})
+
+scrollTop.reveal(
+    `#header, #main .imagem, #main .informacoes, .midias-sociais .midia`,
+    {
+        interval: 500
+    }
+)
+
+const scrollBottom = ScrollReveal({
+    origin: 'bottom',
+    distance: '30px',
+    duration: 500,
+})
+
+scrollBottom.reveal(
+    `#footer`,
+    {
+        interval: 500
+    }
+)
