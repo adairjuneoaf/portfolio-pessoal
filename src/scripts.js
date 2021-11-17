15db1500925b945802c4d0fff47e1777395ae1e1
Adair Juneo <adair_juneo@hotmail.com>
2021-11-16 21:19:09 -0300

    Pag sobre-mim finalizada. Pag Resumo 80%.

--- src/scripts.js
+++ src/scripts.js
@@ -16,6 +16,29 @@ for(const fecharMenu of closeMenu){
     })
 }
 
+const clickMenu = document.querySelectorAll('#header div.tela-menu nav ul li a')
+
+for(const abreFechaMenu of clickMenu){
+    abreFechaMenu.addEventListener('click', ()=>{
+        menu.classList.remove('active')
+        document.body.style.overflow = "visible"
+    })
+}
+
+const optionsMenu = [ "inicio", "sobre", "resumo", "portifolio", "contato" ];
+
+function toggle(sectionSelecionada) {
+    let sections
+    for(let i = 0; i < optionsMenu.length; i += 1) {
+        sections = document.getElementById(optionsMenu[i]);
+        sections.style.display = 'none';
+    }
+    sections = document.getElementById(sectionSelecionada);
+    sections.style.display = 'block';
+}
+
+
+/*
 const scrollTop = ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -41,3 +64,4 @@ scrollBottom.reveal(
         interval: 500
     }
 )
+*/
